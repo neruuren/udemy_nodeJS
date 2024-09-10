4f06602da4a2208cbe73dc849bb1aec09e31c651
gregoryDAY <gregory.day@etik.com>
2024-09-10 16:56:50 +0200

    section 4 : first step with express

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "add Product" page</h1>');
+});
 
-const server = http.createServer(routes.handler);
+app.use('/',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>greetings from Express</h1>');
+});
 
-server.listen(3000);
+app.listen(3000);

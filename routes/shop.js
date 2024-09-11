7ad4fd79a5bbf3ac4eebdd9f59957ad2264f7b0c
gregoryDAY <gregory.day@etik.com>
2024-09-11 17:33:15 +0200

    section 6 : pug implemented

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,15 @@ const express = require('express');
 const rootDir = require('../helpers/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    console.log('shop.js', products);
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    
+    //pug
+    res.render('shop', {prods: products, pageTitle: 'My Book Shop', path: '/'})
 });
 
 module.exports = router;

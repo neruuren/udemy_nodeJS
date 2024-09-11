dc03d7a748a3a8d95ff529ccf54140e4eec2ba31
gregoryDAY <gregory.day@etik.com>
2024-09-11 14:29:19 +0200

    section 5 : dispatch admin and non admin routes, add views, add path, add css

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const rootDir = require('./helpers/path');
 
 const app = express();
 
-app.use('/add-product',(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "add Product" page</h1>');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/',(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>greetings from Express</h1>');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);

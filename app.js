e860f2eff8c4605acf4c40dae92a4340c9a90cd4
gregoryDAY <gregory.day@etik.com>
2024-09-12 12:19:27 +0200

    section 6 : bug correction (error in console)

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not Found!'})
+    res.status(404).render('404', {
+        pageTitle: 'Page not Found!',
+        path: '',
+    });
 });
 
 app.listen(3000);

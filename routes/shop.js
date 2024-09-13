6407f38ddd3a8350f96c2507158929e566de3dbf
gregoryDAY <gregory.day@etik.com>
2024-09-13 13:47:22 +0200

    section 8 : add new pages and routes, refacto controllers, refacto templates

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
 // / => GET
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+// /products => GET
+router.get('/products', shopController.getProducts);
+
+// /cart => GET
+router.get('/cart', shopController.getCart);
+
+// /checkout => GET
+router.get('/checkout', shopController.getCheckout);
 
 
 module.exports = router;

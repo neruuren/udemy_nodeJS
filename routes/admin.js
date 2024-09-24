0c449cb687376334f5bad7934615ca93116195d3
gregoryDAY <gregory.day@etik.com>
2024-09-24 17:29:20 +0200

    section 9 : add route for edit page

--- routes/admin.js
+++ routes/admin.js
@@ -15,5 +15,8 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+// /edit-product => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 
 module.exports = router;

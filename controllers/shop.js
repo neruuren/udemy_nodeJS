2ab5bffd7e89ecaf7a8be5301c97b2edc3823717
gregoryDAY <gregory.day@etik.com>
2024-09-26 19:15:13 +0200

    section 11 : add delete item from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -120,8 +120,16 @@ exports.getCheckout = (req, res, next) => {
 
 exports.postCartDeleteItem = (req, res, next) => {
     const productId = req.body.productId;
-    Product.findById(productId, (product) => {
-        Cart.deleteProduct(productId, product.price);
-        res.redirect('/cart');
-    });
+    req.user.getCart()
+        .then(cart => {
+            return cart.getProducts({ where: { id: productId}})
+        })
+        .then(products => {
+            const product = products[0];
+            return product.cartItem.destroy();
+        })
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err))
 };

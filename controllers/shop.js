40e9cc9019f61a159fd1f0df36d42c10a0fe97ab
gregoryDAY <gregory.day@etik.com>
2024-09-26 18:49:23 +0200

    section 11 : implement the add an item to cart functionnality

--- controllers/shop.js
+++ controllers/shop.js
@@ -73,10 +73,31 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const productId = req.body.productId;
-    Product.findById(productId, (product) => {
-        Cart.addProduct(productId, product.price);
-    })
-    res.redirect('/');
+    let fetchedCart;
+    req.user.getCart()
+        .then(cart => {
+            fetchedCart = cart;
+            return cart.getProducts({ where: { id: productId} });
+        })
+        .then(products => {
+            let product;
+            if (products.length > 0) {
+                product = products[0];
+            }
+            let newQuantity = 1;
+            if (product) {
+                // ...
+            }
+            return Product.findByPk(productId).then(product => {
+                return fetchedCart.addProduct(product, { through: { quantity: newQuantity }})
+            }).catch(err => console.log(err));
+        })
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => {
+            console.log((err));
+        })
 }
 
 exports.getOrders = (req, res, next) => {   

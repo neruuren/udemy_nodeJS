303328c4ac7c76f2f09b9e9fcd80ef4e4063c00f
gregoryDAY <gregory.day@etik.com>
2024-09-26 17:48:41 +0200

    section 11 : use magic association for admin controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -34,8 +34,13 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const productId = req.params.productId
-    Product.findByPk(productId)
-        .then(product => {
+    req.user.getProducts({ where: { id: productId} })
+    // Product.findByPk(productId)
+        .then(products => {
+            const product = products[0];
+            if(!product) {
+                return res.redirect('/');
+            }
             res.render('admin/edit-product', {
                 pageTitle: 'edit product',
                 path: '/admin/edit-product',
@@ -87,7 +92,8 @@ exports.postDeleteProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll()
+    // Product.findAll()
+    req.user.getProducts()
     .then(products => {
         res.render('admin/products', {
             prods: products,

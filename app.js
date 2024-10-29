349a69e8a04a67be5407f8835f0db74524726c9f
gregoryDAY <gregory.day@etik.com>
2024-10-29 17:40:00 +0100

    section 14 : implemment session

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorsController = require('./controllers/errors');
 const rootDir = require('./helpers/path');
 
+const MONGODB_URI = 'mongodb+srv://udemyShoppingApp:OMQ7Qw6eU8gz1Ubf@cluster0.zmacg.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+
 const User = require('./models/user');
 
 const app = express();
+
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -23,16 +32,24 @@ app.use(express.static(path.join(rootDir, 'public')));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User
-        .findById("671eb00577841e4b0a9b20e3")
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => {
-            console.log(err);
-        });
+User
+.findById("671eb00577841e4b0a9b20e3")
+.then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => {
+        console.log(err);
+    });
 });
+app.use(
+    session({
+        secret: 'my secret(should be a long string in  production)', 
+        resave: false, 
+        saveUninitialized: false,
+        store: store,
+    })
+);
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -41,7 +58,7 @@ app.use(authRoutes);
 app.use(errorsController.get404);
 
 mongoose
-    .connect('mongodb+srv://udemyShoppingApp:OMQ7Qw6eU8gz1Ubf@cluster0.zmacg.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+    .connect(MONGODB_URI)
     .then(() => {
         User
             .findOne()
--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,8 @@ const router = express.Router();
 // /login => GET
 router.get('/login', authController.getLogin);
 
+// /login => POST
+router.post('/login', authController.postLogin);
+
 
 module.exports = router;
